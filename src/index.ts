1d6ac32c61255b9d0d1d4c3b25e50cd18a3ee4f0
DIYgod <i@diygod.me>
2022-05-16 18:40:54 +0100

    feat: change default ipfs gateway

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ class Unidata {
         this.options = Object.assign(
             {},
             {
-                ipfsGateway: 'https://gateway.ipfs.io/ipfs/',
+                ipfsGateway: 'https://ipfs.io/ipfs/',
                 alchemyAPIKey: 'demo',
                 web3StorageAPIToken:
                     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDAyMDIwODZmRjU5OUU0Y0YyMzM4MkUzNjg1Y0NmZUEyOGNBODBCOTAiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NTIzNjM1Njk3NDUsIm5hbWUiOiJVbmlkYXRhIn0.XmsAuXvbTj4BFhZlJK4xXfbd0ltVZJCEhqdYcW_kLOo',
