ba78fc19ddd43e018864f4a8fee730ab06d69f22
DIYgod <i@diygod.me>
2022-05-27 05:27:22 +0100

    fix: dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,6 @@ export default defineConfig({
             fileName: (format) => `unidata.${format}.js`,
         },
         sourcemap: true,
-        target: 'es2020',
     },
     define: {
         SDK_VERSION: JSON.stringify(version),
