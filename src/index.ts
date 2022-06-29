ba52b251cfca1909fc127e58f2a684c2f84534a7
DIYgod <i@diygod.me>
2022-06-29 17:41:20 +0100

    fix: window in node env

--- src/index.ts
+++ src/index.ts
@@ -41,7 +41,7 @@ class Unidata {
                 ipfsGateway: 'https://gateway.ipfs.io/ipfs/',
                 web3StorageAPIToken:
                     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDAyMDIwODZmRjU5OUU0Y0YyMzM4MkUzNjg1Y0NmZUEyOGNBODBCOTAiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NTIzNjM1Njk3NDUsIm5hbWUiOiJVbmlkYXRhIn0.XmsAuXvbTj4BFhZlJK4xXfbd0ltVZJCEhqdYcW_kLOo',
-                ethereumProvider: window.ethereum,
+                ...(typeof window !== 'undefined' && { ethereumProvider: window.ethereum }),
             },
             options,
         );
