9e05a530c9ed04bbee72a7a96f59fc3ca990ce97
DIYgod <i@diygod.me>
2022-08-11 13:01:35 +0100

    feat: export ProfileInput LinkInput NoteInput

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import Utils from './utils';
-import ProfilesC from './profiles';
-import LinksC from './links';
+import ProfilesC, { ProfileInput } from './profiles';
+import LinksC, { LinkInput } from './links';
 import AssetsC from './assets';
-import NotesC from './notes';
+import NotesC, { NoteInput } from './notes';
 import type { Asset, Assets, Note, Notes, Link, Links, Profile, Profiles } from './specifications';
 
-export { Asset, Assets, Note, Notes, Link, Links, Profile, Profiles };
+export { Asset, Assets, Note, Notes, Link, Links, Profile, Profiles, ProfileInput, LinkInput, NoteInput };
 
 console.log(
     `${'\n'} %c Unidata v${SDK_VERSION} %c https://unidata.app ${'\n'}`,
